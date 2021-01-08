7bdc4df6cfa44e1261e3b17fc345aef953b11a1d
Youssef-Hossam <Yhmadboly@outlook.com>
2021-01-08 03:05:05 +0200

    Update

--- lib/Ui/Home.dart
+++ lib/Ui/Home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:task_github_repo/Model/UserRepos.dart';
+import 'package:task_github_repo/Utilities/constant.dart';
 import '../Utilities/utility_methods.dart';
-import 'file:///F:/YousF74/YoYo%20stuDy/task_github_repo/lib/Utilities/constant.dart';
 import 'package:task_github_repo/presnter/user_repo_presenter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,6 +13,7 @@ class _HomeScreenState extends State<HomeScreen> implements UserRepoContract {
   UserReposPresenter _userReposPresenter;
   List<UserRepos> _userRepos = [];
   bool isLoading = false;
+  bool loadMore = false;
   ScrollController _scrollController = ScrollController();
   bool _isGrid = false;
 
@@ -53,51 +54,79 @@ class _HomeScreenState extends State<HomeScreen> implements UserRepoContract {
           ),
         ],
       ),
-      body:SingleChildScrollView(
-     child: Column(
-       mainAxisAlignment: MainAxisAlignment.start,
-       crossAxisAlignment: CrossAxisAlignment.stretch,
-       children: [
-       SizedBox(
-       height:8,
-     ),
-        Row(children: [
-          Padding(padding: EdgeInsets.only(left:12.0),
-              child: Text('Hi John',style:titlesStyle,)
-          ),
-        ],
-        ),
-        SizedBox(
-          height:4,
-        ),
-        !isLoading
-            ? Center(
-                child: CircularProgressIndicator(
-                backgroundColor: Colors.black,
-                valueColor: new AlwaysStoppedAnimation<Color>(Colors.cyan),
-              ))
-            : _userRepos.length == 0
-                ? Center(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              height: 8,
+            ),
+            Row(
+              children: [
+                Padding(
+                    padding: EdgeInsets.only(left: 12.0),
                     child: Text(
-                      'No Data to show!!',
-                      style: TextStyle(
-                        color: Colors.black54,
-                        fontWeight: FontWeight.bold,
-                      ),
+                      'Hi John',
+                      style: titlesStyle,
+                    )),
+              ],
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            !isLoading
+                ? Center(
+                    child: CircularProgressIndicator(
+                    backgroundColor: Colors.black,
+                    valueColor: new AlwaysStoppedAnimation<Color>(Colors.cyan),
+                  ))
+                : _userRepos.length == 0
+                    ? Center(
+                        child: Text(
+                          'No Data to show!!',
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    : _isGrid == true
+                        ? buildgridView(context)
+                        : buildlistHorontalView(),
+            !isLoading
+                ? Container()
+                : !loadMore
+                    ? Center(
+                        child: CircularProgressIndicator(
+                        backgroundColor: Colors.transparent,
+                        valueColor:
+                            new AlwaysStoppedAnimation<Color>(Colors.cyan),
+                      ))
+                    :  Container(
+                      margin: const EdgeInsets.only(left:16.0,top:0,right:16.0,bottom:0),
+                      child: RaisedButton(
+                            child: Text(
+                              'Load more',
+                              style: TextStyle(color: Colors.red),
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _loadoMoreData();
+                                loadMore = false;
+                              });
+                            },
+                          ),
                     ),
-                  )
-                : _isGrid == true
-                    ? buildgridView(context)
-                    : buildlistHorontalView(),
-        ],
-     ),
-    ),
+          ],
+        ),
+      ),
     );
   }
 
   Widget buildlistHorontalView() {
     return Container(
-      height: getScreenHeight(context)*0.8,
+      height: getScreenHeight(context) * 0.6,
       child: SingleChildScrollView(
         child: Padding(
             padding: const EdgeInsets.all(2.0),
@@ -110,7 +139,6 @@ class _HomeScreenState extends State<HomeScreen> implements UserRepoContract {
                   return Container(
                     //  child: Container(
                     height: 75,
-                    //width: MediaQuery.of(context).size.width * 0.40,
                     child: Card(
                       child: Container(
                         color: Colors.cyanAccent,
@@ -127,11 +155,11 @@ class _HomeScreenState extends State<HomeScreen> implements UserRepoContract {
                 })),
       ),
     );
-  }  //method to Display list in list view builder
+  } //method to Display list in list view builder
 
   Widget buildgridView(BuildContext context) {
     return Container(
-      height: getScreenHeight(context) * 0.8,
+      height: getScreenHeight(context) * 0.7,
       width: getScreenWidth(context),
       child: Padding(
         padding: EdgeInsets.all(8.0),
@@ -148,19 +176,12 @@ class _HomeScreenState extends State<HomeScreen> implements UserRepoContract {
             ),
             itemBuilder: (BuildContext context, int index) {
               return GestureDetector(
-                onTap: () {
-//                setState(() {
-//                  selectedColor = index;
-//                });
-                },
                 child: Card(
                   color: Colors.greenAccent,
                   shape: RoundedRectangleBorder(
                     side: BorderSide(color: Colors.white70, width: 1),
                     borderRadius: BorderRadius.circular(15),
                   ),
-//                  child: Container(
-//                    color: Colors.lightBlueAccent[100],
                   child: GestureDetector(
                     child: Padding(
                       padding: EdgeInsets.all(8.0),
@@ -177,31 +198,41 @@ class _HomeScreenState extends State<HomeScreen> implements UserRepoContract {
                     onTap: () {},
                   ),
                 ),
-//                ),
               );
             }),
       ),
     );
   } //method to Display list in Grid view builder
+
   _loadData() async {
     await _userReposPresenter.getAllRepos(
         context, Constants.per_Page, Constants.page);
-  }  // Method to Call Api
+  } // // Method to Call Api
+
+  _loadoMoreData() async {
+    await _userReposPresenter.getAllRepos(
+        context, Constants.per_Page, Constants.page++);
+  } // Method to Call Api to get more data (pagination)
+
   @override
   void onGetRepoFaild(onError) {
     setState(() {
       isLoading = true;
     });
     // TODO: implement onGetRepoFaild
-  }  //request failld
+  } //request failld
 
   @override
   void onGetRepoSuccess(List<UserRepos> r) {
     setState(() {
-      _userRepos = r;
-      print('ResponseSuccess' + _userRepos.length.toString());
-      isLoading = true;
-    });
+
+        for (int x = 0; x < r.length; x++) {
+          _userRepos.add(r[x]);
+          //  print('ResponseSuccess' + _userRepos.length.toString());
+          isLoading = true;
+          loadMore = true;
+        }
+      });
     // TODO: implement onGetRepoSuccess
-  }  // request Success
+  } // request Success
 }
--- lib/Utilities/constant.dart
+++ lib/Utilities/constant.dart
@@ -1,8 +1,8 @@
 class Constants {
   String baseUrl;
   String userRepo;
-  static int per_Page = 15  ;
- static int page =1 ;
+  static int per_Page = 15  ; // every call get only 15 index
+ static int page =1 ;  // every time to load more data increase number of page
 
 
 
